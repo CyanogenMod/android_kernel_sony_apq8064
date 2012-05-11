9ec8fcab8fd91bccc609e391a09ebe7f04b1ebec
David Howells <dhowells@redhat.com>
2012-05-11 10:56:56 +0100

    KEYS: Permit in-place link replacement in keyring list
    Make use of the previous patch that makes the garbage collector perform RCU
    synchronisation before destroying defunct keys.  Key pointers can now be
    replaced in-place without creating a new keyring payload and replacing the
    whole thing as the discarded keys will not be destroyed until all currently
    held RCU read locks are released.
    
    If the keyring payload space needs to be expanded or contracted, then a
    replacement will still need allocating, and the original will still have to be
    freed by RCU.
    
    Change-Id: I6c4f784f120951fb51ac9c23856ea37f51770bb9
    Signed-off-by: David Howells <dhowells@redhat.com>

--- security/keys/gc.c
+++ security/keys/gc.c
@@ -137,14 +137,14 @@ void key_gc_keytype(struct key_type *ktype)
  * deallocated under us as only our caller may deallocate it.
  */
 static void key_gc_keyring(struct key *keyring, time_t limit)
-{
+ {
 	struct keyring_list *klist;
+	struct key *key;
 	int loop;
 
 	kenter("%x", key_serial(keyring));
 
-	if (keyring->flags & ((1 << KEY_FLAG_INVALIDATED) |
-			      (1 << KEY_FLAG_REVOKED)))
+	if (test_bit(KEY_FLAG_REVOKED, &keyring->flags))
 		goto dont_gc;
 
 	/* scan the keyring looking for dead keys */
@@ -156,8 +156,9 @@ static void key_gc_keyring(struct key *keyring, time_t limit)
 	loop = klist->nkeys;
 	smp_rmb();
 	for (loop--; loop >= 0; loop--) {
-		struct key *key = rcu_dereference(klist->keys[loop]);
-		if (key_is_dead(key, limit))
+		key = rcu_dereference(klist->keys[loop]);
+		if (test_bit(KEY_FLAG_DEAD, &key->flags) ||
+		    (key->expiry > 0 && key->expiry <= limit))
 			goto do_gc;
 	}
 
