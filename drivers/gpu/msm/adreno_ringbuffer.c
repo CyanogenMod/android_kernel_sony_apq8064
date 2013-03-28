516bf4b137ab3ba45042cbeeb7a4d4e7bafcd343
Tarun Karra <tkarra@codeaurora.org>
2013-03-27 19:37:55 -0700

    msm: kgsl: Allocate space in ringbuffer for EOF commands
    When userspace passes end of frame (EOF) flag, ringbuffer is
    inserted with EOF marker commands. Allocate ringbuffer space
    for EOF marker commands.
    
    CRs-Fixed: 469807
    Change-Id: I7de8d1b81d358a8d8753ac84fbdceddad27bb17d
    Signed-off-by: Tarun Karra <tkarra@codeaurora.org>

--- drivers/gpu/msm/adreno_ringbuffer.c
+++ drivers/gpu/msm/adreno_ringbuffer.c
@@ -577,6 +577,9 @@ adreno_ringbuffer_addcmds(struct adreno_ringbuffer *rb,
 		total_sizedwords += 3; /* global timestamp without cache
 					* flush for non-zero context */
 
+	if (flags & KGSL_CMD_FLAGS_EOF)
+		total_sizedwords += 2;
+
 	ringcmds = adreno_ringbuffer_allocspace(rb, context, total_sizedwords);
 	if (!ringcmds)
 		return -ENOSPC;
