eaf4ab81eaea7b2e728163b70c69491f87892af4
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-08-03 15:42:31 +0200

    fusion3: fix gpio_spi_cs_config
    Change-Id: Ie802077bc7a0ae97572ee9277f0e31405643fa82

--- arch/arm/mach-msm/board-sony_yuga-gpiomux.c
+++ arch/arm/mach-msm/board-sony_yuga-gpiomux.c
@@ -64,6 +64,7 @@ struct msm_gpiomux_config apq8064_ethernet_configs[] = {
 	},
 };
 #endif
+
 /* Chip selects for SPI clients */
 static struct gpiomux_setting gpio_spi_cs_config = {
 	.func = GPIOMUX_FUNC_GPIO,
